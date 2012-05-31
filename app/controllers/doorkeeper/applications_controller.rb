fe38bff188975c446d6be2fc3191f24b45d05b3c
cameron <cameron@api.gy>
2012-05-31 11:52:21 -0700

    break up the logic for debugging

--- app/controllers/doorkeeper/applications_controller.rb
+++ app/controllers/doorkeeper/applications_controller.rb
@@ -14,11 +14,14 @@ module Doorkeeper
 
     def create
       @application = Application.new(params[:application])
-      if @application.owner_id == current_resource_owner.id && @application.save
-        redirect_to @application, :status => 200
+      if @application.save
+        if @application.owner_id == current_resource_owner.id
+          redirect_to @application, :status => 200
+        else
+          render :new
+        end
       else
-        redirect_to edit_applications_url
-        #render :status => 500, :nothing => true
+        render :new
       end
     end
     
@@ -39,8 +42,7 @@ module Doorkeeper
     def destroy
       @application = Application.find_by_uid_and_owner_id(params[:id], current_resource_owner.id)
       flash[:notice] = "Application deleted" if @application.destroy
-      redirect_to 
-      
+      redirect_to applications_url
     end
   end
 end
